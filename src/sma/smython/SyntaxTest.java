bfefba77684f0b343495de5bf39c219574704ff5
sma <eibaan at gmail.com>
2009-04-25 12:59:41 +0200

    syntax test now generates 520 single test cases and looks so much nicher in my IDE's junit test runner

--- src/sma/smython/SyntaxTest.java
+++ src/sma/smython/SyntaxTest.java
@@ -5,56 +5,65 @@ package sma.smython;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class SyntaxTest {
-  private int failures, files;
-
-  private void readAndParse(File file) throws Exception {
-    System.out.print(file + "...");
-    files += 1;
-    
-    // read file (hopefully, it's UTF-8)
-    Reader r = new InputStreamReader(new FileInputStream(file), "utf-8");
-    StringBuilder b = new StringBuilder();
-    char[] buf = new char[4096];
-    int len = r.read(buf);
-    while (len != -1) {
-      b.append(buf, 0, len);
-      len = r.read(buf);
-    }
-    r.close();
-    // now try to parse it
-    try {
-      parse(b.toString());
-      System.out.println("ok.");
-    } catch (Exception e) {
-      //System.out.println("FAILURE.");
-      failures += 1;
-    }
+  public static junit.framework.Test suite() {
+    TestSuite testSuite = new TestSuite("SyntaxTest");
+    testSuite.addTest(createTests("Parse all py files", new File("/Users/sma/python3/lib/python3.0")));
+    testSuite.addTest(createTests("Parse all py test files", new File("/Users/sma/python3/lib/python3.0/test")));
+    return testSuite;
   }
 
-  private void parse(String source) throws Exception {
-    Parser parser = new Parser(new Scanner(source + "\n"));
-    try {
-      parser.parseFileInput();
-    } catch (Exception e) {
-      System.out.println("ERROR in line " + parser.line());
-      e.printStackTrace(System.out);
-      throw e;
+  private static junit.framework.Test createTests(String name, File dir) {
+    TestSuite testSuite = new TestSuite(name);
+    for (File file : dir.listFiles(new FilenameFilter() {
+      public boolean accept(File file, String name) {
+        return name.endsWith(".py");
+      }
+    })) {
+      testSuite.addTest(new SyntaxTestCase(file));
     }
+    return testSuite;
   }
 
-  @Test
-  public void parseAllFilesOfThePython3Distribution() throws Exception {
-    for (File file : new File("/Users/sma/python3/lib/python3.0").listFiles()) {
-      if (file.getName().endsWith(".py")) {
-        readAndParse(file);
+  static class SyntaxTestCase extends TestCase {
+    private final File file;
+
+    SyntaxTestCase(File file) {
+      super(file.getName());
+      this.file = file;
+    }
+
+    @Override
+    protected void runTest() throws Throwable {
+      parse(read(file));
+    }
+
+    private static String read(File file) throws IOException {
+      Reader r = new InputStreamReader(new FileInputStream(file), "utf-8");
+      StringBuilder b = new StringBuilder();
+      char[] buf = new char[4096];
+      int len = r.read(buf);
+      while (len != -1) {
+        b.append(buf, 0, len);
+        len = r.read(buf);
+      }
+      r.close();
+      return b.toString();
+    }
+
+    private static void parse(String source) throws Exception {
+      Parser parser = new Parser(new Scanner(source + "\n"));
+      try {
+        parser.parseFileInput();
+      } catch (Exception e) {
+        System.err.println("ERROR in line " + parser.line());
+        throw e;
       }
     }
-    System.out.println(failures + " failures of " + files + " files");
   }
 }
